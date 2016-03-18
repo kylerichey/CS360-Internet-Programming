796e7dc73dffb79be60184ca12b9f57f8ceeb969
Kyle Richey <kylerichey@users.noreply.github.com>
2016-03-18 17:20:27 +0000

    project9 done no creative

---
+++ project9/public/javascripts/app.js
@@ -0,0 +1,50 @@
+angular.module('comment', []) 
+.controller('MainCtrl', [ 
+	'$scope','$http', 
+	function($scope,$http){ 
+		$scope.test = 'Hello world!';
+
+ $scope.comments = [
+		 {title:'Comment 1', upvotes:5}, 
+		 {title:'Comment 2', upvotes:6}, 
+		 {title:'Comment 3', upvotes:1},
+		 {title:'Comment 4', upvotes:4},
+		 {title:'Comment 5', upvotes:3}
+		 ];
+		 		
+		 $scope.addComment = function() { 
+		// $scope.comments.push({title:$scope.formContent,upvotes:0}); $scope.formContent=''; };
+		  $scope.create({ 
+			title: $scope.formContent, 
+			upvotes: 0,
+			}); 
+			$scope.formContent = '';
+			};
+			
+		  $scope.incrementUpvotes = function(comment) { 
+			//comment.upvotes += 1; 
+			$scope.upvote(comment);
+		  };
+		  
+		   $scope.getAll = function() {
+			   return $http.get('/comments').success(function(data){
+				   angular.copy(data, $scope.comments);
+				   });
+				 };
+				 
+		  $scope.create = function(comment) {
+			  return $http.post('/comments', comment).success(function(data){ 
+			  $scope.comments.push(data); 
+			  }); 
+			  };
+			  
+			   $scope.upvote = function(comment) {
+				   return $http.put('/comments/' + comment._id + '/upvote') .success(function(data){
+					   console.log("upvote worked"); 
+					   comment.upvotes += 1; }); };
+			  
+		  $scope.getAll();
+		  
+	} 
+	
+]);
